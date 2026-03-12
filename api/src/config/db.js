ed01176f7d20fe8ba552a1f8e97c3fb0c5844a86
houssamWaked <houssamalwaked@gmail.com>
2026-03-12 04:16:02 +0200

    Update database password in db.js

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',
-    password: 'Husamoo12',
+    password: 'postgres',
     database: 'csis_279_spring_26_db',
     port: 5432,
 });
